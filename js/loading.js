af07650259fa9957c052e0904d610100ad785caa
aila <ailafeka@gmail.com>
2024-03-03 16:02:24 +0800

    修正loading畫面，避免loading狀態可以滾動

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,13 @@
 const loading = document.getElementById('loading');
 const loading_end = document.getElementById('loading_end');
+const main = document.getElementById('main');
 
 window.addEventListener('load',function(){
     loading_end.style.transform='scale(15,15) rotate(135deg)';
-    console.log('載入完成');
+
 });
 loading_end.addEventListener('transitionend',function(){
     loading.style.opacity = 0;
+    main.classList.remove('main_hidden');
     setTimeout(function(){loading.style.display='none';},500)
 })
